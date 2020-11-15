faadcbc10bb805b20be8b81590dfebbd7a7c6b14
maxim.or <maximor17s@gmail.com>
2020-11-15 06:23:43 +0200

    product page

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,61 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Card, Image, ListGroup, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-const ProductScreen = () => {
+
+
+const ProductScreen = ({ match }) => {
+
+    const product = products.find(product => product._id == match.params.id)
 
     return (
-        <div>
-            product screen
-        </div>
+        <>
+            <Link className='btn btn-dark my-3 ' to='/' >
+                go back
+            </Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid />
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant="flush" className='text-center'>
+                        <ListGroup.Item>
+                            <h3><strong>{product.name}</strong></h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            Price: {product.price}$
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            {product.description}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <ListGroup className='text-center'>
+                        <ListGroup.Item>
+                            Price: {product.price}$
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            status: {product.countInStock > 1 ? 'in stock' : 'out of stock'}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Button
+                                type='button'
+                                className='btn btn-block'
+                                disabled={product.countInStock == 0}
+                            >
+                                Add to cart
+                            </Button>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+            </Row>
+        </>
     )
 }
 
