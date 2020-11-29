7ec7a5727b93351c416689d0d43c32c5b8af59d1
maxim.or <maximor17s@gmail.com>
2020-11-30 00:22:01 +0200

    cart reducer & action & Blank screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect, } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { addToCart } from '../actions/cartAction'
 
 const CartScreen = () => {
+
+    // const dispatch = useDispatch()
+
+    // useEffect(() => {
+    //     dispatch(addToCart())
+    // }, [dispatch])
+
     return (
         <div>
             cart screen
