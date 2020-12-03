2efc4e150b4e3cef048b6ae3f7111cd1f3475a1d
maxim.or <maximor17s@gmail.com>
2020-12-03 17:07:23 +0200

    user registration & custom authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ import User from '../models/userModel.js'
 import generateToken from '../utils/generateToken.js'
 
 // @desc .... Auth user & get token
-// @route ... GET /api/users/login
+// @route ... POST /api/users/login
 // @access .. Public
 const authUser = asyncHandler(async (req, res) => {
 
@@ -29,7 +29,64 @@ const authUser = asyncHandler(async (req, res) => {
 
 })
 
+// @desc .... Auth user & get token
+// @route ... GET /api/users/profile
+// @access .. Private
+const getUserProfile = asyncHandler(async (req, res) => {
+
+    if (req.user) {
+        const { user } = req
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+// @desc .... register new user 
+// @route ... POST /api/users
+// @access .. Public
+const registerUser = asyncHandler(async (req, res) => {
+
+    const { name, email, password } = req.body
+
+    const userExist = await User.findOne({ email })
+    if (userExist) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(404)
+        throw new Error('Invalid user data ')
+    }
+
+
+
+})
+
+
 export {
     authUser,
-
+    getUserProfile,
+    registerUser
 }
