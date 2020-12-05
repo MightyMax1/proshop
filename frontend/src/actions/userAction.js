2e9c0197a115da4be7c692edd6a84d0a4a5e2e70
maxim.or <maximor17s@gmail.com>
2020-12-05 23:59:44 +0200

    navbar -  logout

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -29,3 +29,8 @@ export const login = (email, password) => async (dispatch) => {
         })
     }
 }
+
+export const logout = () => async (dispatch) => {
+    dispatch({ type: USER_LOGOUT })
+    localStorage.removeItem("userInfo")
+}
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,21 @@
 import React from 'react'
-import { Container, Navbar, Nav } from 'react-bootstrap'
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { useSelector, useDispatch } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import { logout } from '../actions/userAction'
+
 
 const Header = () => {
+
+    const { userInfo } = useSelector((state) => state.userLogin)
+
+    const dispatch = useDispatch()
+
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
+
     return (
         <header>
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
@@ -16,9 +29,20 @@ const Header = () => {
                             <LinkContainer to="/cart">
                                 <Nav.Link >Cart <i className='fas fa-shopping-cart'></i></Nav.Link>
                             </LinkContainer>
-                            <LinkContainer to="/login">
-                                <Nav.Link >Sing in <i className='fas fa-user'></i></Nav.Link>
-                            </LinkContainer>
+                            {userInfo ? (
+                                <NavDropdown title={userInfo.name} id='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={logoutHandler}>
+                                        logout
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
+                                    <LinkContainer to="/login">
+                                        <Nav.Link >Sing in <i className='fas fa-user'></i></Nav.Link>
+                                    </LinkContainer>
+                                )}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
