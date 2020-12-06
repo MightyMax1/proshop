cf05fc866a91865071fb4fbe4efaa4bddf4fcdd4
maxim.or <maximor17s@gmail.com>
2020-12-06 18:40:49 +0200

    register screen and functionality

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT } from '../constants/userConstant.js'
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from '../constants/userConstant.js'
 
 export const login = (email, password) => async (dispatch) => {
     try {
@@ -33,4 +33,40 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => async (dispatch) => {
     dispatch({ type: USER_LOGOUT })
     localStorage.removeItem("userInfo")
+}
+
+export const register = (name, email, password) => async (dispatch) => {
+    try {
+        dispatch({ type: USER_REGISTER_REQUEST })
+
+        const config = {
+            headers: {
+                'Content-Type': 'applocation/json'
+            },
+            name,
+            email,
+            password
+        }
+        const { data } = await axios.post('/api/users/', config);
+
+        dispatch({
+            type: USER_REGISTER_SUCCESS,
+            payload: data
+        })
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+    } catch (error) {
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message
+        })
+    }
 }
