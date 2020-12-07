d456d7ab5bc360c130b3d9cc836f56c79a0f0d27
maxim.or <maximor17s@gmail.com>
2020-12-07 02:28:06 +0200

    user update profile backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -84,9 +84,43 @@ const registerUser = asyncHandler(async (req, res) => {
 
 })
 
+// @desc ....update user profile
+// @route ... PUT /api/users/profile
+// @access .. Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+
+    const user = await User.findById(req.user._id)
+    console.log('user', user)
+
+    if (user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save()
+
+        console.log('updatedUser', updatedUser)
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(updatedUser._id),
+        })
+
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
 
 export {
     authUser,
     getUserProfile,
-    registerUser
+    registerUser,
+    updateUserProfile
 }
