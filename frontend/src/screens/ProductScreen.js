15f876e7cf70d489a8edd2ba32e9c091efafd4a7
maxim.or <maximor17s@gmail.com>
2020-12-21 23:30:27 +0200

    add review product screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { Row, Col, Card, Image, ListGroup, Button, Form } from 'react-bootstrap'
+import { Row, Col, Card, Image, ListGroup, Button, Form, } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
@@ -29,13 +29,25 @@ const ProductScreen = ({ match, history }) => {
 
 
     useEffect(() => {
-
         dispatch(detailsProduct(match.params.id))
-
-    }, [match.params.id, dispatch])
+        dispatch({ type: PRODUCT_CREATE_REVIEW_RESET })
+        if (success) {
+            alert('Review Submitted')
+            setRating(0)
+            setComment('')
+        }
+    }, [match.params.id, dispatch, success])
 
     const addToCartHandler = () => {
         history.push(`/cart/${match.params.id}?qty=${qty}`)
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(createProductReview(match.params.id, {
+            rating,
+            comment
+        }))
     }
 
     return (
@@ -124,6 +136,42 @@ const ProductScreen = ({ match, history }) => {
                                             <p>{review.comment}</p>
                                         </ListGroup.Item>
                                     ))}
+                                    <ListGroup.Item>
+                                        <h3>Write a Customer Review</h3>
+                                        {errorReview && <Message variant='danger'>{errorReview}</Message>}
+                                        {userInfo
+                                            ? (
+                                                <Form onSubmit={submitHandler}>
+                                                    <Form.Group controlId='rating'>
+                                                        <Form.Label>Rating</Form.Label>
+                                                        <Form.Control as='select' value={rating} onChange={(e) => setRating(e.target.value)}>
+                                                            <option value=''>Select...</option>
+                                                            <option value='1'>1 - Poor</option>
+                                                            <option value='2'>2 - Fair</option>
+                                                            <option value='3'>3 - Good</option>
+                                                            <option value='4'>4 - Very Good</option>
+                                                            <option value='5'>5 - Excellent</option>
+                                                        </Form.Control>
+                                                    </Form.Group>
+                                                    <Form.Group controlId='comment'>
+                                                        <Form.Label>Comment</Form.Label>
+                                                        <Form.Control
+                                                            as='textarea'
+                                                            row='3'
+                                                            value={comment}
+                                                            onChange={(e) => setComment(e.target.value)}
+                                                        ></Form.Control>
+                                                    </Form.Group>
+                                                    <Button type='submit' variant='primary'>
+                                                        Submit
+                                                    </Button>
+                                                </Form>
+                                            )
+                                            : <Message>
+                                                Please <Link to='/login'>Sign in</Link> to write a review
+                                             </Message>
+                                        }
+                                    </ListGroup.Item>
                                 </ListGroup>
                             </Col>
                         </Row>
